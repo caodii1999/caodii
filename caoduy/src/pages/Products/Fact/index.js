f21dc084cc49d8d920eeba8676156e5f388101d4
cao duy <caoduy771@gmail.com>
2023-02-07 09:03:36 +0700

    first commit

---
+++ caoduy/src/pages/Products/Fact/index.js
@@ -0,0 +1,35 @@
+import styles from './Facts.module.scss';
+import classNames from 'classnames/bind';
+import TypewriterComponent from 'typewriter-effect';
+import { useState, useEffect } from 'react';
+
+import { facts } from '../../../data';
+const cx = classNames.bind(styles);
+function Fact() {
+    const [fact, setFact] = useState(0);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setFact(fact === facts.length - 1 ? 0 : fact + 1);
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [fact]);
+    return (
+       <div className={cx('wrapper')}>
+            <TypewriterComponent
+                options={{
+                    strings: [...facts],
+                    autoStart: true,
+                    loop: true,
+                    pauseFor: 1000,
+                    delay: 15,
+                    deleteSpeed: 15,
+                    cursor: ''
+                  }}
+            />
+       </div>
+    );
+}
+
+export default Fact;
